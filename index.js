023856aa8324b40589bed7a3ae20fcc84835b9a1
shakhawath <m.shakhawath18@gmail.com>
2023-06-04 11:38:32 +0600

    handle request & response function

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
+// dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('node:string_decoder');
 
-// app object - module scaffolding
+// app object - module scafoulding
 const app = {};
 
-// cinfiguration
+// configuration
 app.config = {
     port: 3000,
 };
 
-// create server
+// creat server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, () => {
         console.log(`listening to port ${app.config.port}`);
     });
 };
-// handle req res
+
+// handle request and response
 app.handleReqRes = (req, res) => {
-    res.end('hi');
+    // request handling
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimedPath = path.replace(/^\/+|\/+$/g, '');
+
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Wellcome to our website');
+    });
 };
 
-// sart the server
+// start the server
 app.createServer();
