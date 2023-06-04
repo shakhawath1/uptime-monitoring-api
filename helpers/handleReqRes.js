4415157cdf6c100932bbcd5ccf168194c88c649a
shakhawath <m.shakhawath18@gmail.com>
2023-06-04 12:03:49 +0600

    refactor the code

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,35 @@
+// dependencies
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+
+// module scaffoulding
+const handler = {};
+
+// handle request & response
+handler.handleReqRes = (req, res) => {
+    // request handling
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimedPath = path.replace(/^\/+|\/+$/g, '');
+
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Wellcome to our website');
+    });
+};
+
+exports.module = handler;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // dependencies
 const http = require('http');
-const url = require('url');
-const { StringDecoder } = require('node:string_decoder');
+const { handleReqRes } = require('./helpers/handleReqRes');
 
-// app object - module scafoulding
+// app object - module scaffoulding
 const app = {};
 
 // configuration
@@ -20,31 +19,7 @@ app.createServer = () => {
 };
 
 // handle request and response
-app.handleReqRes = (req, res) => {
-    // request handling
-    // get the url and parse it
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimedPath = path.replace(/^\/+|\/+$/g, '');
-
-    const method = req.method.toLowerCase();
-    const queryStringObject = parsedUrl.query;
-    const headersObject = req.headers;
-
-    const decoder = new StringDecoder('utf-8');
-    let realData = '';
-
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer);
-    });
-
-    req.on('end', () => {
-        realData += decoder.end();
-        console.log(realData);
-        // response handle
-        res.end('Wellcome to our website');
-    });
-};
+app.handleReqRes = handleReqRes;
 
 // start the server
 app.createServer();
