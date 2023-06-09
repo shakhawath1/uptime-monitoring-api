850005d6c46095044dbfa9a14e90de7fae8b9c27
shakhawath <m.shakhawath18@gmail.com>
2023-06-09 15:57:30 +0600

    data file ready for creating new data

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
+
+// testing
+data.create('test', 'newF', { name: 'Shakhawath', country: 'Bangladesh' }, (err) => {
+    console.log(err);
+});
 
 // creat server
 app.createServer = () => {
