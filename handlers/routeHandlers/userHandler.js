1db72c58a53937cdeef9c881d217568f0e867979
shakhawath <m.shakhawath18@gmail.com>
2023-06-12 13:06:30 +0600

    added the function of read the user data

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -7,7 +7,7 @@
  */
 
 // dependencies
-const { hash } = require('../../helpers/utilities');
+const { hash, parseJSON } = require('../../helpers/utilities');
 const data = require('../../lib/data');
 // const { parseJson } = require('../../helpers/utilities');
 
@@ -97,7 +97,33 @@ handler._users.post = (requestProperties, callback) => {
 
 // get user
 handler._users.get = (requestProperties, callback) => {
-    callback(200);
+    // check the phone number is valide
+    const phone =
+    typeof requestProperties.queryStringObject.phone === 'string' &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+        ? requestProperties.queryStringObject.phone
+        : false;
+
+    if(phone){
+        // lookup the user
+        data.read('users', phone,(err, u)=>{
+            const user ={...parseJSON(u)};
+
+            if(!err && user){
+                delete user.password;
+                callback(200, user)
+            }else{
+                callback(404,{
+                    'error':'Requestd user was not found'
+                })
+            }
+
+        })
+    }else{
+        callback(404,{
+            'error':'Requestd user was not found'
+        })
+    }
 };
 
 // update user
