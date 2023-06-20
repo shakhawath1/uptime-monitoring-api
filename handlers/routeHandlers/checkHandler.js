9dc6400a805ac5a437ccaac286a5bbd603f22698
shakhawath <m.shakhawath18@gmail.com>
2023-06-20 23:59:10 +0600

    added check function

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -28,6 +28,10 @@ handler._check = {};
 
 // create check
 handler._check.post = (requestProperties, callback) => {
+    let protocol = typeog(requestProperties.body.protocol === 'string' && ['http', 'https'].indexOf(requestProperties.body.protocol) > -1 ? requestProperties.body.protocol : false)
+    let url = typeog(requestProperties.body.url === 'string' && requestProperties.body.url.trim().length > 0 ? requestProperties.body.url : false)
+    let method = typeog(requestProperties.body.method === 'string' && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false)
+    let successCods = typeog(requestProperties.body.successCods === 'object' && requestProperties.body.successCods instanceof Array ? requestProperties.body.successCods : false)
 };
 
 // get check
